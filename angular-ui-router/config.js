42f092ae433e03382edf09de1e097f9a244733c9
zhouyanggodking <zhou.yang.god.king@gmail.com>
2015-10-19 23:23:51 +0800

    mutliple views

--- angular-ui-router/config.js
+++ angular-ui-router/config.js
@@ -9,12 +9,9 @@
                 url: '/home',
                 templateUrl: 'views/home.html'
             })
-            .state('about', {
-                url: '/about',
-                templateUrl: 'views/about.html'
-            })
+
             .state('home.list', {
-                url:'/list',
+                url: '/list',
                 templateUrl: 'views/home-list.html',
                 controller: function ($scope) {
                     $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
@@ -23,9 +20,46 @@
              .state('home.paragraph', {
                  url: '/paragraph',
                  templateUrl: 'views/home-paragraph.html',
-                 controller: function($scope){
+                 controller: function ($scope) {
                      $scope.text = "Godking paragraph";
                  }
              })
+            .state('about', {
+                url: '/about',
+                views: {
+
+                    // the main template will be placed here (relatively named)
+                    '': { templateUrl: 'views/about.html' },
+
+                    // the child views will be defined here (absolutely named)
+                    'columnOne@about': { template: 'Look I am a column!' },
+
+                    // for column two, we'll define a separate controller 
+                    'columnTwo@about': {
+                        templateUrl: 'views/table-data.html',
+                        controller: function ($scope) {
+
+                            $scope.message = 'test';
+
+                            $scope.scotches = [
+                                {
+                                    name: 'Macallan 12',
+                                    price: 50
+                                },
+                                {
+                                    name: 'Chivas Regal Royal Salute',
+                                    price: 10000
+                                },
+                                {
+                                    name: 'Glenfiddich 1937',
+                                    price: 20000
+                                }
+                            ];
+
+                        }//end controller
+                    }
+                }
+
+            });
     };
 });
