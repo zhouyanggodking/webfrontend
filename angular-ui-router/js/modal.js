bb378024d44e909994f28406d934a260aaf6ab5d
zhouyanggodking <zhou.yang.god.king@gmail.com>
2015-11-22 20:47:16 +0800

    angular reset changes

--- angular-ui-router/js/modal.js
+++ angular-ui-router/js/modal.js
@@ -1,6 +1,10 @@
 ﻿define(['jquery'], function ($) {
     var fc = function ($scope) {
         $('#tokenfield').tokenfield();
+        $scope.reset = function () {
+            $scope.signup.name = null;
+        }
+
     };
 
     fc.$inject = ['$scope'];
