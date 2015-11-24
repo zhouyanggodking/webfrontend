515a10db97c206fa4f994019fbe0a0e772b34606
zhouyanggodking <zhou.yang.god.king@gmail.com>
2015-11-25 00:27:27 +0800

    ng-repeat delete

--- angular-ui-router/js/modal.js
+++ angular-ui-router/js/modal.js
@@ -5,6 +5,14 @@
             $scope.signup.name = null;
         }
 
+        //$scope.urls = ["www.baidu.com", "www.google.com", "tttttttttttttttttttttttttt"];
+
+        $scope.urls = ["www.baidu.com", "www.baidk.com", "www.baidw.com"];
+
+        $scope.remove = function (array, index) {
+            array.splice(index, 1);
+        }
+
     };
 
     fc.$inject = ['$scope'];
