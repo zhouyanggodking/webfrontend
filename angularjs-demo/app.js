1c9a0ddbccff45e5320b66238a9c1c303f682631
sxzhou <sxzhou@advent.com>
2015-11-30 23:45:54 -0800

    godking directive

--- angularjs-demo/app.js
+++ angularjs-demo/app.js
@@ -1,4 +1,4 @@
-﻿define(['ui.router', 'angular', 'config', 'js/controllers/service-demo', 'js/services/itemService', 'js/controllers/directive-demo'], function (uiRouter, angular, config, serviceDemoCtrl, itemService, directiveDemoCtrl) {
+﻿define(['ui.router', 'angular', 'config', 'js/controllers/service-demo', 'js/services/itemService', 'js/controllers/directive-demo', 'js/directives/godkingDirective'], function (uiRouter, angular, config, serviceDemoCtrl, itemService, directiveDemoCtrl, godkingDirective) {
     var app = angular.module('app', ['ui.router']);
 
     app.config(config);
@@ -10,5 +10,12 @@
     //app.factory('Items', itemService);   //factory( name,  $getFunc())
 
     app.service('Items', itemService);     //service( name , contructor())
+
+
+    ///////////////directives////////////////////
+
+    app.directive('godkingDirective', godkingDirective);
+
+    ///////////////directives ends//////////////
     return app;
 });
--- angularjs-demo/js/directives/godkingDirective.js
+++ angularjs-demo/js/directives/godkingDirective.js
@@ -1,3 +1,21 @@
 ﻿define([], function () {
+    var fn = function () {
+        var directiveDef = {
+            restrict: 'E',
+            template: '<div>directive-demo</div>',
+            //templateUrl,
+            replace: true,
+            transclude: false,
+            scope: false,
+            controller: function ($scope, $element, $attrs, $transclude) { },
+            //require: ^?direciveName
+            link: function(scope, iElement, iAttr){}
+        };
 
+        return directiveDef;
+    };
+
+    fn.$inject = [];
+
+    return fn;
 });
