9f99f1cafb492a37fbca6439ba78e2d3870d3182
zhouyanggodking <zhou.yang.god.king@gmail.com>
2016-03-22 22:09:22 +0800

    missed file app.js

--- playground/src/app.js
+++ playground/src/app.js
@@ -1,4 +1,4 @@
-﻿define(['ui.router', 'angular', 'state-config', 'views/app.views'],
+﻿define(['ui.router', 'angular', 'state-config', 'app.views'],
     function (uiRouter, angular, stateConfig) {
         var app = angular.module('app', ['ui.router', 'app.views']);
         app.config(stateConfig);
