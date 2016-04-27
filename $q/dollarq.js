614e1d62500da31c12b451241b302f37a1535554
sxzhou <sxzhou@advent.com>
2016-04-27 16:45:31 +0800

    async method

--- $q/dollarq.js
+++ $q/dollarq.js
@@ -1,6 +1,14 @@
 ﻿define(['angular'], function (angular) {
     angular.module('dollarq', [])
-        .controller('dollarqCtrl', function ($q) {
-            console.log('king');
+        .controller('dollarqCtrl', function ($q, $http) {
+
+            getPersonAsync(function (response) {
+                console.log(response);
+            });
+
+
+            function getPersonAsync(success, failed) {
+                $http.get('data/person.json').then(success, failed);
+            }
         });
 });
