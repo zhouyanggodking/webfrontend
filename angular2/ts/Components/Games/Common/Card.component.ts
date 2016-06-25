4639e635fb90a5faa12883ba374569f24acce508
zhouyanggodking <zhou.yang.god.king@gmail.com>
2016-06-25 16:49:36 +0800

    card component

---
+++ angular2/ts/Components/Games/Common/Card.component.ts
@@ -0,0 +1,22 @@
+﻿import { Component } from '@angular/core';
+import {CardService} from '../../../Services/Card.Service'
+import {Card} from '../../../Model/card/Card'
+import {CardFace} from '../../../Model/card/CardFace'
+import {CardSuite} from '../../../Model/card/CardSuite'
+
+@Component({
+    selector: 'card',
+    templateUrl: 'template/Components/Games/Common/Card.template.html',
+    inputs: ['face', 'suite'],
+    providers: [CardService]
+})
+export class CardComponent {
+    face: CardFace;
+    suite: CardSuite;
+
+    get cardSrc()  {
+        return CardService.getCardPNGImage(new Card(this.suite, this.face));
+    }
+
+
+}
--- angular2/ts/Components/Games/JacksOrBetter/JacksOrBetter.component.ts
+++ angular2/ts/Components/Games/JacksOrBetter/JacksOrBetter.component.ts
@@ -3,11 +3,15 @@ import {CardService} from '../../../Services/Card.Service'
 import {Card} from '../../../Model/card/Card'
 import {CardFace} from '../../../Model/card/CardFace'
 import {CardSuite} from '../../../Model/card/CardSuite'
+import {CardComponent} from '../../../Components/Games/Common/Card.Component'
 
 @Component({
     selector: 'jacks-or-better',
-    templateUrl: 'template/Components/Games/JacksOrBetter/jacksorbetter.template.html'
+    templateUrl: 'template/Components/Games/JacksOrBetter/JacksOrBetter.template.html',
+    directives: [CardComponent]
 })
 export class JacksOrBetterComponent {
-    cardsrc = CardService.getCardPNGImage(new Card(CardSuite.Diamonds, CardFace.King));
+    //cardsrc = CardService.getCardPNGImage(new Card(CardSuite.Diamonds, CardFace.King));
+    suite = CardSuite.Clubs;
+    face = CardFace.Queen;
 }
