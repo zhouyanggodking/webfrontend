4835936cdd66ef1023aa1a391fb9ae9528f53ca8
zhouyanggodking <zhou.yang.god.king@gmail.com>
2016-06-26 10:55:22 +0800

    card component

--- angular2/ts/Components/Games/Common/Card.component.ts
+++ angular2/ts/Components/Games/Common/Card.component.ts
@@ -4,15 +4,18 @@ import {Card} from '../../../Model/card/Card'
 import {CardFace} from '../../../Model/card/CardFace'
 import {CardSuite} from '../../../Model/card/CardSuite'
 
+//usage: <card [face] ="face" [suite]="suite" width="500"></card>
+
 @Component({
     selector: 'card',
     templateUrl: 'template/Components/Games/Common/Card.template.html',
-    inputs: ['face', 'suite'],
+    inputs: ['face', 'suite', 'imageWidth: width'],
     providers: [CardService]
 })
 export class CardComponent {
     face: CardFace;
     suite: CardSuite;
+    imageWidth: string;
 
     get cardSrc()  {
         return CardService.getCardPNGImage(new Card(this.suite, this.face));
