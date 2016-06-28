4380f628f89ea79c6f3ed7f04ef92c0a9be268df
sxzhou <sxzhou@advent.com>
2016-06-28 16:48:20 +0800

    basic types

--- typescript/helloworld.ts
+++ typescript/helloworld.ts
@@ -1,5 +1,22 @@
-﻿function greet(text) {
-    console.log(text);
-}
+﻿let isPerson: boolean = true;
+let age: number = 40;
+let fullname: string = 'Ocean Sky';
+let statement = `Name: ${fullname}, Age: ${age}`;
 
-greet('hello world');
+console.log(statement);
+//array declaration
+let arrStr: string[] = ['one', 'two', 'three'];
+let arrInt: Array<number> = [1, 2, 3];
+let arrAny: Array<any> = ['one', 2, 'three'];
+
+console.log(arrAny);
+//tuple
+let x: [string, number] = ["OceanSky", 40];
+console.log(x[0]);
+x[0] = 'Pacific';
+console.log(x[0]);
+
+//type assertion
+let testAny: any = 'Ocean';
+let str: string = <string>testAny;
+str = testAny as string;
