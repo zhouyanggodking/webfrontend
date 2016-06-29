8571b5604e61173b00d810409f2ecd536e0ab3dc
sxzhou <sxzhou@advent.com>
2016-06-29 16:41:28 +0800

    function basics

--- typescript/helloworld.ts
+++ typescript/helloworld.ts
@@ -1,33 +1,68 @@
-﻿//for destructing demo
-//array destructing
-let [one, two] = [1, 2];
-//console.log(one);
-//console.log(two);
-//swap trick
-[two, one] = [one, two];
-//console.log(one);
-//console.log(two);
+﻿//for function demo
 
-let [first, ...rest] = [1, 2, 3, 4, 5];
-//console.log(first);
-//console.log(rest);
-//console.log(rest[0]);
-
-let [, , three] = [1, 2, 3, 4, 5];
-//console.log(three);
-
-/////////////////////////////////////////////////
-//object destructing
-let o = {
-    a: "foo",
-    b: 12,
-    c: "bar"
+//declaration
+function add(x: number, y: number): number {
+    return x + y;
 }
-let {a, c} = o;
-//console.log(a);
-//console.log(b);
-//let {ae, be} = o;
 
-let {a: foo, c: bar} = o;
-//console.log(foo);
-//console.log(bar);
+let aliasAdd = add;
+
+//funciton type
+let myAdd: (x: number, y: number) => number
+    = add;
+
+//optional parameters: optionsal params must follow required ones
+function compile(filename: string, extension?: string): boolean {
+    return true;
+}
+
+//function compile2(extension?: string, filename: string): boolean {
+//    return true;
+//}
+
+//default parameters
+function cppCompile(filename: string, extension: string = '.cpp'): boolean {
+    return true;
+}
+
+//function cppCompile2(extension: string = '.cpp', filename: string): boolean {
+//    return true;
+//}
+//cppCompile2(undefined, 'helloworld');
+
+//rest params: treated as boundless optional parameters
+function addEx(x: number, ...restNum: number[]): number {
+    let sum = x;
+    for (let i = 0; i < restNum.length; ++i) {
+        sum += restNum[i];
+    }
+    return sum;
+}
+//console.log(addEx(1, 2, 3, 4, 5, 6, 7, 8));
+
+//////////////////////////////////////////////////////////
+//function overload
+function concat(x: string, y: string): string;
+function concat(x: number, y: number): number;
+function concat(x: any, y: any): any {
+    if (typeof x == 'number' && typeof y == 'number') {//1, 2 => 12
+        return +(x.toString() + y.toString());
+    }
+    return x.toString() + y.toString();
+}
+
+//lambda expression
+function accumulate(numList: number[], action: (x: number, y: number) => number): number{
+    let acc: number;
+    for (let i = 0; i < numList.length; ++i) {
+        if (i > 0) {
+            acc = action(acc, numList[i]);
+        } else {
+            acc = numList[0];
+        }
+    }
+    return acc;
+}
+
+console.log(accumulate([1, 2, 3, 4], add))
+console.log(accumulate([1, 2, 3, 4], (x, y) => { return x * y }));
