afa61832227ff66d7ebdc810c40c86519f772ead
zhouyanggodking <zhou.yang.god.king@gmail.com>
2016-07-02 13:44:38 +0800

    export/import syntax

---
+++ typescript/Person.ts
@@ -0,0 +1,24 @@
+﻿class Person {
+    name: string;
+    age: number;
+    display(): void {
+        console.log(`name: ${this.name}, age: ${this.age}`);
+    }
+}
+
+export {Person};
+//export default Person;
+//export {Person as PersonAlias}
+
+//function export
+export function print(obj: any): void {
+    console.log(obj);
+}
+
+//variable export
+const version: string = '1.0.0';
+
+export {version};
+
+
+
---
+++ typescript/main.ts
@@ -0,0 +1,9 @@
+﻿import {Person, print, version} from './Person'
+//import {Person as PersonAlias} from './Person'
+
+let p = new Person();
+p.name = 'OceanSky';
+p.age = 90;
+p.display();
+
+print(version);
