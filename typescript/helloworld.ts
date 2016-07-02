039f92af769fb217c697fa0640d485858455870e
zhouyanggodking <zhou.yang.god.king@gmail.com>
2016-07-02 10:48:02 +0800

    for...in  vs for...of

--- typescript/helloworld.ts
+++ typescript/helloworld.ts
@@ -1,61 +1,22 @@
-﻿//for generic demo
-//generic function
-function getType<T>(identity: T): string{
-    return typeof identity;
+﻿let arr = [5, 6, 3, 4];
+
+for (let v of arr) {
+    console.log(v);
 }
 
-let type = getType('str');
-type = getType<number>(9);
-type = getType({ name: 'king' });
-type = getType([1, 2, 3, 5]);
-
-console.log(type);
-
-
-//generic type
-let myGetType: <T>(identity: T) => string = getType;
-type = myGetType(true);
-
-//generic class/interface
-class Stack<T>{
-    private _list: Array<T>;
-    constructor(list: Array<T>) {
-        this._list = list;
-    }
-    push(elem: T): void {
-        this._list.push(elem);
-    }
-
-    pop(): void {
-        this._list.pop();
-    }
-
-    top(): T {
-        let length = this._list.length;
-        return this._list[length - 1];
-    }
-}
-//generic constraint
-interface IAnimal { }
-class Animal { }
-class Fish extends Animal { }
-class AnimalStack<T extends Animal/*IAnimal*/>{
-    private _list: Array<T>;
-    constructor(list: Array<T>) {
-        this._list = list;
-    }
-    push(elem: T): void {
-        this._list.push(elem);
-    }
-
-    pop(): void {
-        this._list.pop();
-    }
-
-    top(): T {
-        let length = this._list.length;
-        return this._list[length - 1];
-    }
+for (let key in arr) {
+    console.log(key);
 }
 
-class Demo<T extends U, U>{ }
+
+let obj = {
+    name: 'oceansky',
+    age: 90
+};
+
+//for (let v of obj) {//error, must be list
+//}
+
+for (let key in obj) {
+    console.log(key);
+}
