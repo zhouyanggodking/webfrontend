595c28509d0342f72e7de202a036339ced5b84ec
zhouyanggodking <zhou.yang.god.king@gmail.com>
2016-07-02 20:56:10 +0800

    namespace of demo

--- typescript/Person.ts
+++ typescript/Person.ts
@@ -1,24 +1,20 @@
-﻿class Person {
-    name: string;
-    age: number;
-    display(): void {
-        console.log(`name: ${this.name}, age: ${this.age}`);
+﻿namespace Creature {
+    export class Person {
+        name: string;
+        age: number;
+        display(): void {
+            console.log(`name: ${this.name}, age: ${this.age}`);
+        }
     }
+
+    //function export
+    export function print(obj: any): void {
+        console.log(obj);
+    }
+
+    //variable export
+    export const version: string = '1.0.0';
 }
-
-export {Person};
-//export default Person;
-//export {Person as PersonAlias}
-
-//function export
-export function print(obj: any): void {
-    console.log(obj);
-}
-
-//variable export
-const version: string = '1.0.0';
-
-export {version};
 
 
 
--- typescript/main.ts
+++ typescript/main.ts
@@ -1,9 +1,12 @@
-﻿import {Person, print, version} from './Person'
+﻿///<reference path='Person.ts'/>
+//import {Person, print, version} from './Person'
 //import {Person as PersonAlias} from './Person'
 
+
+import Person = Creature.Person;
 let p = new Person();
 p.name = 'OceanSky';
 p.age = 90;
 p.display();
 
-print(version);
+Creature.print(Creature.version);
