f26c5c636a83e431f333e80ed1d2f41cda9f931b
zhouyanggodking <zhou.yang.god.king@gmail.com>
2016-08-01 23:53:34 +0800

    Router parameters

--- angular2-demo/EntityService.ts
+++ angular2-demo/EntityService.ts
@@ -2,20 +2,33 @@
 
 @Injectable()//used for service depending on other services
 export class EntityService {
+    private entityList = [
+        {
+            id: '1',
+            longName: 'Advent Software Corporation',
+            shortName: 'ADV'
+        },
+        {
+            id: '2',
+            longName: 'International Business Machine',
+            shortName: 'IBM'
+        },
+        {
+            id: '3',
+            longName: 'Microsoft Corporation',
+            shortName: 'MSFT'
+        }
+    ];
     get() {
-        return [
-            {
-                longName: 'Advent Software Corporation',
-                shortName: 'ADV'
-            },
-            {
-                longName: 'International Business Machine',
-                shortName: 'IBM'
-            },
-            {
-                longName: 'Microsoft Corporation',
-                shortName: 'MSFT'
+        return this.entityList;
+    }
+
+    getById(id: string) {
+        for (let entity of this.entityList) {
+            if (id === entity.id) {
+                return entity;
             }
-        ];
+        }
+        return null;
     }
 }
--- angular2-demo/RouteConfig.ts
+++ angular2-demo/RouteConfig.ts
@@ -1,11 +1,13 @@
 ﻿import {provideRouter, RouterConfig} from '@angular/router';
 import { AppComponent } from './AppComponent';
 import {EntityList} from './EntityList';
+import {Entity} from './Entity';
 import {About} from './About';
 
 const routes: RouterConfig = [
     { path: 'about', component: About },
-    { path: 'entityList', component: EntityList }
+    { path: 'entityList', component: EntityList },
+    { path: 'entity/:id', component: Entity }
 ];
 
 export const appRouteProviders = [
