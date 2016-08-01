d8c36529beab675f9be7764103f2638a68e873d2
sxzhou <sxzhou@advent.com>
2016-08-01 15:47:02 +0800

    directive demo - Inputs

--- angular2-demo/DoNothingDirective.ts
+++ angular2-demo/DoNothingDirective.ts
@@ -1,10 +1,41 @@
-﻿import {Directive} from '@angular/core'
+﻿import {Directive, Input} from '@angular/core'
 
 @Directive({
-    selector: '[doNothing]'
+    selector: '[doNothing]',
+    inputs: ['text: logText']//accepts array of strings
 })
 export class DoNothingDirective {
-    constructor() {
-        console.log('do nothing');
+    set text(value) {
+        console.log(value);
     }
 }
+//different forms
+//@Directive({
+//    selector: '[doNothing]',
+//    inputs: ['logText']//accepts array of strings
+//})
+//export class DoNothingDirective {
+//    set logText(value) {
+//        console.log(value);
+//    }
+//}
+
+//@Directive({
+//    selector: '[doNothing]'    
+//})
+//export class DoNothingDirective {
+//    @Input('logText')
+//    set text(value) {
+//        console.log(value);
+//    }
+//}
+
+//@Directive({
+//    selector: '[doNothing]'    
+//})
+//export class DoNothingDirective {
+//    @Input('')
+//    set logText(value) {
+//        console.log(value);
+//    }
+//}
