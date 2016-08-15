a3017ab91c8ceca47ff703d04a7bb2efb07a0fc0
zhouyanggodking <zhou.yang.god.king@gmail.com>
2016-08-15 23:18:06 +0800

    observer

---
+++ rxjs-demo/observer.js
@@ -0,0 +1,28 @@
+﻿//basic way to create observable and observer, then connect
+//var observable = Rx.Observable.create(function (observer) {
+//    observer.onNext('king');
+//    observer.onNext('ocean');
+//    observer.onNext('sky');
+//    observer.onCompleted();//done
+//});
+
+//var observer = Rx.Observer.create(
+//    function onNext(x) { console.log(x);},
+//    function onError(err) { },
+//    function onCompleted() { console.log('done'); }
+//    );
+
+//observable.subscribe(observer);
+
+//using operator
+//from arg
+//var observable = Rx.Observable.of('king');
+//from iterable
+//var observable = Rx.Observable.from(['king', 'ocean', 'sky']);
+//from event
+var observable = Rx.Observable.fromEvent(document, 'mousemove');
+observable.subscribe(
+    function (x) { console.log(x); },
+    function (err) { },
+    function () { console.log('done')}
+    )
