75892e5a809f5345a17fa65f50e68340a523864c
sxzhou <sxzhou@advent.com>
2016-10-09 17:29:48 +0800

    protractor test

---
+++ protractor/e2e/first-test.js
@@ -0,0 +1,12 @@
+﻿describe('first test', function () {
+    it('first case', function () {
+        browser.get('http://localhost:9033/app/index.html');
+        var name = element(by.model('name'));
+        var url = browser.getCurrentUrl();
+        console.log(url);
+        name.clear();
+        name.sendKeys('test');
+        expect(name.getAttribute('value')).toBe('test');
+        browser.pause();
+    });
+});
