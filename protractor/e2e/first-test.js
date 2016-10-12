2236fd00c514d21692a819a26be5bfcb39546350
sxzhou <sxzhou@advent.com>
2016-10-12 09:56:04 +0800

    protractor: test non-angular web site

--- protractor/e2e/first-test.js
+++ protractor/e2e/first-test.js
@@ -1,5 +1,5 @@
-﻿describe('first test', function () {
-    it('first case', function () {
+﻿describe('angular web site test', function () {
+    it('angular web site case', function () {
         browser.get('http://localhost:9090/tamaleweb/login');
         var name = element(by.model('inputUsr'));
         var pwd = element(by.model('inputPwd'));
@@ -7,7 +7,26 @@
         name.sendKeys('sawyer');
         pwd.clear();
         pwd.sendKeys('123');
-        expect(name.getAttribute('value')).toBe('test');
+        expect(name.getAttribute('value')).toBe('sawyer');
+        //Show as Names
+        element(by.buttonText('LOG IN')).click();
+        //expect(browser.getCurrentUrl()).toBe('http://localhost:9090/tamaleweb/research');
+        element(by.buttonText('MENU')).click();
+        element(by.id('btnNewEntity')).click();
         browser.pause();
+    });
+});
+
+
+describe('non angular web site test', function () {
+    beforeEach(function () {
+        return browser.ignoreSynchronization = true;
+    });
+    it('non angular web site case', function () {
+        browser.get('https://github.com');
+        element(by.linkText('Explore')).click();
+        element(by.linkText('Sign up for free')).click();
+        browser.pause();
+
     });
 });
--- protractor/protractor.config.js
+++ protractor/protractor.config.js
@@ -1,9 +1,10 @@
-﻿exports.config = {
+﻿var HtmlReporter = require('protractor-html-screenshot-reporter');
+exports.config = {
     /**
      * Use `seleniumAddress` for faster startup; run `./node_modules/.bin/webdriver-manager start` to launch the Selenium server.
      * Use `seleniumPort` to let Protractor manage its own Selenium server instance (using the server JAR in its default location).
      */
-    seleniumAddress: 'http://localhost:4444/wd/hub',//if comment this out, protractor will start and end webdriver-manager automaticaly
+    //seleniumAddress: 'http://localhost:4444/wd/hub',//if comment this out, protractor will start and end webdriver-manager automaticaly
     // seleniumPort: 4444,
 
     /**
@@ -18,11 +19,19 @@
      * Properties passed to Selenium -- see https://code.google.com/p/selenium/wiki/DesiredCapabilities for more info.
      */
     capabilities: {
-        'browserName': 'chrome'
+        'browserName': 'chrome',
+        'chromeOptions': {
+            'args': ['disable-extensions', 'start-maximized']
+        }
     },
 
     /**
      * This should point to your running app instance, for relative path resolution in tests.
      */
-    baseUrl: 'http://localhost:9090'
+    baseUrl: 'http://localhost:9090',
+    onPrepare: function () {//ref: http://programmerbuddy.blogspot.com/2014/03/full-automation-of-protractor-e2e-tests_22.html
+        jasmine.getEnv().addReporter(new HtmlReporter({
+            baseDirectory: 'screenshots'
+        }));
+    }
 };
