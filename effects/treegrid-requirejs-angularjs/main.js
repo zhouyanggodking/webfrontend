0384f9b00781aeb7b9847e658b2b9a142511de3c
godking <zhou.yang.god.king@gmail.com>
2017-02-28 21:23:45 +0800

    working example

---
+++ effects/treegrid-requirejs-angularjs/main.js
@@ -0,0 +1,16 @@
+﻿require.config({
+    paths: {
+        'angular': './node_modules/angular/angular',
+        'jquery': './node_modules/jquery/dist/jquery',
+        'treeGrid': './Grid/GridE'
+    },
+    shim: {
+        'angular': {
+            exports: 'angular'
+        }
+    }
+});
+
+require(['angular', './appModule', 'jquery'], function (angular) {
+    angular.bootstrap(document, ['app']);
+});
---
+++ effects/treegrid-requirejs-angularjs/treeGridModule.js
@@ -0,0 +1,52 @@
+﻿define(['angular', 'treeGrid'], function (angular) {
+    angular.module('app.treeGrid', [])
+    .controller('treeGridCtrl', treeGridContrller)
+
+    function treeGridContrller(){
+        var data = {
+            "Cfg": {
+                "id": "basic"
+            },
+            "LeftCols": [
+              {
+                  "Name": "LEFT1",
+                  "Width": "200"
+              }
+            ],
+            "Cols": [
+              {
+                  "Name": "DATA1"
+              },
+              {
+                  "Name": "DATA2"
+              }
+            ],
+            "Header": {
+                "LEFT1": "Left Fixed Column",
+                "DATA1": "Data1 Column",
+                "DATA2": "Data2 Column"
+            },
+            "Body": [
+                [
+                    {
+                        "LEFT1": "1",
+                        "DATA1": "GOD",
+                        "DATA2": "KING"
+                    },
+                    {
+                        "LEFT1": "2",
+                        "DATA1": "Ocean",
+                        "DATA2": "Sky"
+                    },
+                    {
+                        "LEFT1": "3",
+                        "DATA1": "Jerry",
+                        "DATA2": "Jones"
+                    }
+                ]
+            ]
+        };
+
+        TreeGrid({ Data: { Data: data } }, 'main');
+    }
+});
