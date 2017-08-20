c3809bcade9d59f5e7270b8a5372a2a65c8f7a6f
godking <zhou.yang.god.king@gmail.com>
2017-08-20 18:26:54 +0800

    basic vue features

--- vue-demo/app/main.js
+++ vue-demo/app/main.js
@@ -19,6 +19,75 @@ var main = new Vue({
         }, {
             text: 'c++',
             show: false
-        }]
+        }],
+        userName: '',
+        password: '',
+        skills: [],
+        gender: 'Male',
+        productName: 'Casinolink',
+        productNameList: [],
+        conditional: '',
+        pageCount: 5,
+        godking: {
+            userName: 'godking',
+            email: 'god.king@godking.com',
+            corporation: {
+                name: 'IGT',
+                country: 'US'
+            }
+        },
+        forWatcher: ''
+    },
+    computed: {
+        reversedProductname: function() {
+            return this.productName.split('').reverse().join('');
+        },
+        now: function() {
+            return Date.now();
+        }
+    },
+    watch: {
+        forWatcher: function(newVal, oldVal) {
+            console.log('New Value: ' + newVal + ',Old Value: ' + oldVal);
+            var up = this.$options.filters.upper(newVal);
+            console.log(up);
+        }
+    },
+    methods: {
+        logIn: function(userName, password, event) {
+            console.log(userName + ':' + password);
+            console.log(event);
+        },
+        buttonGrandParentClick: function() {
+            console.log('grand parent div');
+        },
+        buttonParentClick: function() {
+            console.log('parent div');
+        },
+        buttonClick: function() {
+            console.log('button click');
+        }
+    },
+    filters: {
+        postfix: function(value) {
+            return value + 'GODKING';
+        },
+        upper: function(value) {
+            return value.toUpperCase();
+        }
+    },
+    created: function() {
+        console.log(this.message);
+        console.log('created');
+    },
+    mounted: function() {
+        console.log(this.now);
+        console.log('mounted');
+    },
+    updated: function() {
+        console.log('updated');
+    },
+    beforeDestroy: function() {
+        prompt('Are you sure to destroy?');
     }
 });
