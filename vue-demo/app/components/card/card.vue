f4a24c09e6a9f94f32453433a10f23b7c68a6ba4
godking <zhou.yang.god.king@gmail.com>
2017-08-27 21:51:57 +0800

    card component

---
+++ vue-demo/app/components/card/card.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h1>Card</h1>
+        <h2>{{face}} - {{suit}}</h2>
+        <img v-bind:src="cardSrc" v-bind:alt="`${cardSrc} not found`">
+    </div>
+  
+</template>
+
+<script>
+export default {
+    name: 'card',
+    data(){
+        return {
+            
+        };
+    },
+    props:{
+        face:{
+            type: Number,
+            required: true
+        },
+        suit:{
+            type: String,
+            required: true
+        }
+    },
+    computed:{
+        cardSrc: function(){
+            return `./img/png/${this.face}_of_${this.suit}s.png`
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- vue-demo/app/components/poker.vue
+++ vue-demo/app/components/poker.vue
@@ -1,10 +1,17 @@
 <template>
-  <h1>Texas Holdem</h1>
+  <div>
+    <h1>Texas Holdem</h1>
+    <card v-bind:face="1" suit="spade"></card>
+  </div>  
 </template>
 
 <script>
+import Card from './card/card.vue'
 export default {
-    name: 'poker'
+    name: 'poker',
+    components: {
+        Card
+    }
 }
 </script>
 
