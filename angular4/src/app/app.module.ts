c7c97ccd7a54b8e3e5f689ccafda31069868f96d
Zhou, Sawyer <sxzhou@advent.com>
2017-09-28 05:30:53 -0400

    added basic routing

--- angular4/src/app/app.module.ts
+++ angular4/src/app/app.module.ts
@@ -1,19 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CustomerListComponent } from './components/customer-list/customer-list.component';
 import { CustomerDetailComponent } from './components/customer-detail/customer-detail.component';
 import { LoginComponent } from './components/login/login.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+const appRoutes : Routes = [//first win strategy
+  {path: 'login', component: LoginComponent},
+  {path: '', redirectTo: 'login', pathMatch: 'full'},//init page
+  {path: '**', component: PageNotFoundComponent} //last resort
+];
+
 @NgModule({
   declarations: [
     AppComponent,
     CustomerListComponent,
     CustomerDetailComponent,
-    LoginComponent
+    LoginComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
