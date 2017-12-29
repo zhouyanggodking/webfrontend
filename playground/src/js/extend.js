201175b4b142bce9176f8a0c6953ee926ed18215
godking <zhou.yang.god.king@gmail.com>
2017-12-29 22:59:14 +0800

    jquery extend

---
+++ playground/src/js/extend.js
@@ -0,0 +1,21 @@
+window.extend = function(){
+    let target = arguments[0] || {};
+    for(let index = 1; index < arguments.length; ++index){
+        let source = arguments[index];
+        for(let key in source){
+            let copy = source[key];
+            let clone = target[key];
+            //recusive copy
+            if(toString.call(copy) === '[object Object]'){
+                clone = clone || {};
+                target[key] = extend(clone, copy);
+            }else if(Array.isArray(copy)){
+                clone = clone || [];
+                target[key] = extend(clone, copy);
+            }else{
+                target[key] = copy;
+            }
+        }        
+    }
+    return target;
+};
