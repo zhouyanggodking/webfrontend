9ef06b7b431e97d23dfd803011a7f6803354483a
Zhou, Jing <zhou.yang.god.king@gmail.com>
2018-03-27 07:07:02 -0400

    [g2]: chart axis

--- g2-demo/src/intro.js
+++ g2-demo/src/intro.js
@@ -20,18 +20,41 @@ const chart = new G2.Chart({
   //   lineWidth: 3,
   //   radius: 7
   // },
-  plotBackground: {
-    fill: '276b1a',
-    fillOpacity: 0.8,
-    stroke: 'red',
-    strokeOpacity: 1,
-    opacity: 1,
-    lineWidth: 3,
-    radius: 7
-  }
+  // plotBackground: {
+  //   fill: '276b1a',
+  //   fillOpacity: 0.8,
+  //   stroke: 'red',
+  //   strokeOpacity: 1,
+  //   opacity: 1,
+  //   lineWidth: 3,
+  //   radius: 7
+  // }
 });
 
 chart.source(data);
 chart.coord().transpose();
+chart.axis('genre', {
+  line: {
+    stroke: '#00ff00',
+    lineWidth: 4
+  }  
+});
+chart.axis('sold', {
+  line: {
+    stroke: '#00ffff',
+    lineWidth: 4
+  },
+  // title: {
+  //   position: 'center'
+  // },
+  grid: {
+    align: 'end', //start, center, end
+    type: 'polygon',
+    lineStyle: {
+      stroke: 'red',
+      lineWidth: 2
+    }
+  }
+});
 chart.interval().position('genre*sold').color('genre');
 chart.render();
