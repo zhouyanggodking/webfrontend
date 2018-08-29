eaa13466701950b04b7b8616e700ce538f83b1b0
zhouyanggodking <zhou.yang.god.king@gmail.com>
2018-08-29 11:49:25 +0800

    nuxt demo

---
+++ nuxt-demo/components/Navigation.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>TOP NAVIGATION</div>
+</template>
+
+<script>
+export default {
+  name: 'navigation'
+};
+</script>
---
+++ nuxt-demo/pages/SlotMachine.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <navigation></navigation>
+    <h1>Slot Machine</h1>
+  </div>
+</template>
+
+<script>
+import Navigation from '@/components/Navigation';
+export default {
+  components: {
+    Navigation
+  }
+};
+</script>
