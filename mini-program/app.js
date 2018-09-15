b85f13834bae78484b4860e9f7f5e805978b5d0a
zhouyanggodking <zhou.yang.god.king@gmail.com>
2018-09-15 21:38:45 +0800

    mini-program

---
+++ mini-program/app.js
@@ -0,0 +1,14 @@
+App({
+  onLaunch: function(options) {
+
+  },
+  onShow: function(options) {
+
+  },
+  onHide: function () {
+    // Do something when hide.
+  },
+  onError: function (msg) {
+    console.log(msg)
+  }
+})
