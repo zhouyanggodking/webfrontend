505629852979ce7604b61e65413d399765b56100
zhoujing <zhou.yang.god.king@gmail.com>
2018-11-01 17:48:33 +0800

    just pure html js css files

--- nuxt-demo/pages/index.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h1>First Page</h1>
-</template>
--- requirejs/bootstrapAngular.js
+++
@@ -1,8 +0,0 @@
-﻿define(['require', 'angularjs', 'jquery', 'app'], function (require, angular, $, app) {
-    //require([''], function () {
-    $(document).ready(function () {
-
-        angular.bootstrap(document, ['app']);
-    });
-    //})
-})
