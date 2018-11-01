80d6b986d98c503fd0aaf9173c73e8ee9d8cf817
zhoujing <zhou.yang.god.king@gmail.com>
2018-11-01 17:50:28 +0800

    nodejs

--- lottery/app.js
+++
@@ -1,7 +0,0 @@
-﻿define(['ui.router', 'angular', 'config', 'js/controller/home-controller'], function (uiRouter, angular, config, homeCtrl) {
-    var app = angular.module('app', ['ui.router']);
-
-    app.config(config);
-    app.controller('homeCtrl', homeCtrl);
-    return app;
-});
