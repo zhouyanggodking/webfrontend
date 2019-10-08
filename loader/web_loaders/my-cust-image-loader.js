06973f604515e1fcf26714cd66e6b594b75d474c
zhoujing <zhou.yang.god.king@gmail.com>
2019-10-08 15:59:41 +0800

    sync callback

--- loader/web_loaders/my-cust-image-loader.js
+++ loader/web_loaders/my-cust-image-loader.js
@@ -1,6 +1,7 @@
 module.exports = function(content, map, meta) {
   console.log(content);
-  return `module.exports = ${JSON.stringify(content)}`
+  // return `module.exports = ${JSON.stringify(content)}`
+  this.callback(null, `module.exports = ${JSON.stringify(content)}`, map, meta)
 };
 
 // module.exports.raw = true
