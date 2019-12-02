11b3885cac34dab53aad14dc1800a7605e6192a8
zhoujing <zhou.yang.god.king@gmail.com>
2019-12-02 19:47:56 +0800

    deep clone

---
+++ deep-clone/deepClone.js
@@ -0,0 +1,39 @@
+// need to consider array, reg, date, object.prototype, etc  circular reference?
+
+const deepClone = obj => {
+  if (typeof obj !== obj || typeof obj !== 'function') { // primitive checks
+    return obj;
+  }
+
+  if (obj instanceof RegExp) {
+    const reg = new RegExp(obj.source, obj.flags);
+    reg.lastIndex = reg.lastIndex;
+    return reg;
+  } 
+
+  if (obj instanceof Date) {
+    return new Date(obj.getTime());
+  }
+
+  if (obj instanceof Array) {
+    const arr = [];
+    for (item of obj) {
+      arr.push(deepClone(item));
+    }
+    return arr;
+  }
+
+  // other types???
+
+  // normal objects or user defined objects
+  const proto = Object.getPrototypeOf(obj);
+  const clone = Object.create(proto);
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      clone[key] = deepClone(obj[key]);
+    }
+  }
+  return clone;
+};
+
+module.exports = deepClone
---
+++ deep-clone/deepCloneTest.js
@@ -0,0 +1,36 @@
+const deepClone = require('./deepClone');
+
+const arr = [1, 2, 3];
+arr[10] = 10;
+
+const reg = /abc/igm;
+
+const obje = {
+  name: 'king',
+  arr: [1, 2]
+}
+
+
+function Company() {
+  this.ticker = 'ADVS';
+}
+
+const comp = new Company;
+
+const date = new Date;
+
+const obj = {
+  arr,
+  reg,
+  obje,
+  comp,
+  date
+};
+
+
+const clone = deepClone(obj)
+console.log(clone)
+clone.arr[5] = 5;
+clone.obje.arr[10] = 10;
+clone.date.setMonth(4);
+console.log(clone)
